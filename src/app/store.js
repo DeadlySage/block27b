0bbf9dd8a6fb8f07c8acf89068e12b7904a3ad86
DeadlySage <adrian.hartanto159@gmail.com>
2025-04-07 10:41:32 -0700

    initial commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import reducer from "../features/transactions/transactionsSlice";
 
 // TODO: Configure the store to use the reducer from the transactions slice.
-const store = configureStore();
+const store = configureStore({
+  reducer: reducer
+});
 
 export default store;
